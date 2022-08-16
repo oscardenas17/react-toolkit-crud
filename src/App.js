0eba5600b2afff3809804f8cd28bc21a791c2b16
oscardenas17 <oscardenas17@gmail.com>
2022-08-16 14:34:25 -0500

    redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
-import './App.css';
+import "./App.css";
+import { useSelector } from "react-redux";
+import TaskForm from "./app/components/TaskForm";
+import TaskList from "./app/components/TaskList";
 
 function App() {
+  const taskState = useSelector((state) => state.tasks);
+  console.log(taskState);
+
   return (
     <div className="App">
-  <h1>hello</h1>
+      <h1>hello</h1>
+
+      <TaskForm/>
+      <TaskList/>
+
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import tasksReducer from "../features/tasks/taskSlice";
 
-const store = configureStore({
-     
-})
-
-
-export default store
+export const store = configureStore({
+  reducer: {
+     tasks: tasksReducer,
+  },
+});
